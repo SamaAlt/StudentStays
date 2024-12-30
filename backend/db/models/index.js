2ab2b043cd2979bc2b52ffa3fb038af5db129ae4
SamaAlt <althiabatsamai@gmail.com>
2024-12-30 03:04:36 -0500

    to deploy

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -1,36 +1,3 @@
-// 'use strict';
-
-// const fs = require('fs');
-// const path = require('path');
-// const { Sequelize, DataTypes } = require('sequelize');
-// const config = require(path.resolve(__dirname, '../../config/database.js'))[process.env.NODE_ENV || 'development'];
-// const basename = path.basename(__filename);
-// const db = {};
-
-// // Initialize Sequelize instance
-// const sequelize = new Sequelize(config);
-
-// fs.readdirSync(__dirname)
-//   .filter(file => {
-//     return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
-//   })
-//   .forEach(file => {
-//     const model = require(path.join(__dirname, file))(sequelize, DataTypes);
-//     db[model.name] = model;
-//   });
-
-// // Establish model associations
-// Object.keys(db).forEach(modelName => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// });
-
-// db.sequelize = sequelize;
-// db.Sequelize = Sequelize;
-
-// module.exports = db;
-
 
 'use strict';
 
